e8009c3976374b24578b7ef82dac145bce698a66
superleaf1995 <jesusantonio30122016@gmail.com>
2021-05-07 14:32:42 -0400

    Fixes and stuff

--- src/province.hpp
+++ src/province.hpp
@@ -2,18 +2,17 @@
 #define PROVINCE_H
 #include "pop.hpp"
 
-struct Province {
+class Province {
+public:
 	std::string name;
 	std::string ref_name;
 	uint32_t color;
 	
 	uint64_t population;
 	float budget;
-
+	
 	std::vector<Industry> industries;
-
 	std::vector<Product> products;
-
     std::vector<Pop> pops;
 };
 
