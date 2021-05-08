b4451b459c95cebb805c1a87f3461e0d51c81a58
superleaf1995 <jesusantonio30122016@gmail.com>
2021-05-08 04:46:50 -0400

    add deliver and order system

--- src/economy.hpp
+++ src/economy.hpp
@@ -13,8 +13,15 @@ public:
 	bool is_transport;
 	bool is_retailer;
 	bool is_industry;
-	std::vector<float> relations;
 	std::vector<size_t> operating_provinces;
+
+	bool in_range(size_t prov_id) {
+		// Check that province is in our operational list
+		for(const auto& op: this->operating_provinces) {
+			if(op == prov_id) return true;
+		}
+		return false;
+	}
 };
 
 class Good {
@@ -31,20 +38,26 @@ public:
 	std::vector<size_t> outputs;
 };
 
+class World;
 class Industry {
 public:
 	size_t owner_id;
 	size_t type_id;
 	std::vector<size_t> stockpile;
+
+	bool can_do_output();
+	void add_to_stock(World * world, size_t good_id, size_t add);
 };
 
 class Product {
 public:
-	size_t owner_id;
-	size_t industry_id;
-	size_t good_id;
-	float price;
-	float price_vel;
+	size_t owner_id; // Onwer (companyId) of this product
+	size_t origin_id; // Origin province (where this product was made)
+	size_t industry_id; // Industry in province that made this product
+	size_t good_id; // Good that this product is based on
+	float price; // Price of the product
+	float price_vel; // Velocity of change of price of the product
+	size_t quality; // Quality of the product
 	size_t supply;
 	size_t demand;
 };
