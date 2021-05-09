03254d737dfa45109efde0b47d5cbb0583d9719c
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-09 19:47:30 +0200

    Fixes towards MSVC build

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,6 @@
 
 void rendering_main(void);
 
-#ifdef WINDOWS
-#include <windows.h>
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszArgument, int iShow) {
-	char * argv[1];
-	argv[0] = "/";
-	main(1, argv);
-}
-#endif
-
 #include <chrono>
 #include <thread>
 #include <thread>
@@ -34,3 +25,12 @@ int main(int argc, char ** argv) {
 #endif
 	return 0;
 }
+
+#ifdef WIN32
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszArgument, int iShow) {
+	char* argv[1];
+	argv[0] = "/";
+	main(1, argv);
+}
+#endif
