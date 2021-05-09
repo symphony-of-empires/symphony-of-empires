b809bc62617a6d6a27a3da3bd13a278bf720cac2
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-09 11:59:11 +0200

    economy.cpp: Add const

--- src/economy.cpp
+++ src/economy.cpp
@@ -8,7 +8,7 @@ bool Industry::can_do_output() {
 	return true;
 }
 
-void Industry::add_to_stock(World * world, size_t good_id, size_t add) {
+void Industry::add_to_stock(World * world, const size_t good_id, const size_t add) {
 	for(size_t i = 0; i < stockpile.size(); i++) {
 		if(world->industry_types[this->type_id].inputs[i] == good_id) {
 			this->stockpile[i] += add;
