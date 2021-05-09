57a88a9078f23574b662ae639a8826c73eb85e31
superleaf1995 <jesusantonio30122016@gmail.com>
2021-05-09 19:41:10 -0400

    Add more error checking

--- src/economy.cpp
+++ src/economy.cpp
@@ -1,13 +1,16 @@
 #include "economy.hpp"
 #include "world.hpp"
 
+/// Checks if the industry can do output (if it has enough input)
 bool Industry::can_do_output() {
 	for(const auto& stock: this->stockpile) {
-		if(!stock) return false;
+		if(!stock)
+			return false;
 	}
 	return true;
 }
 
+/// Adds a good by id to a industries stockpile
 void Industry::add_to_stock(World * world, const size_t good_id, const size_t add) {
 	for(size_t i = 0; i < stockpile.size(); i++) {
 		if(world->industry_types[this->type_id].inputs[i] == good_id) {
