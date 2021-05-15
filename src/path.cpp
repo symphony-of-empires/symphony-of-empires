6ef6254ed9e76166b89e420b0047a7b17560f65e
superleaf1995 <jesusantonio30122016@gmail.com>
2021-05-15 04:37:04 -0400

    Some fixes and more documented code

--- src/path.cpp
+++ src/path.cpp
@@ -1,40 +1,38 @@
-#include <string>
-#include <iostream>
+#include "path.hpp"
 #ifndef WIN32
-#include <unistd.h>
+#	include <unistd.h>
+#else
+#	include <Libloaderapi.h>
 #endif
 #include <limits.h>
 
-#ifdef WIN32
-#	include <Libloaderapi.h>
-#endif
+namespace Path {
+	static inline std::string get_exec_path(void) {
+	#ifdef WIN32
+		char buf[PATH_MAX];
+		const auto len = GetModuleFileNameA(nullptr, buf, sizeof(buf) - 1);
+	#else
+		char buf[PATH_MAX];
+		ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf) - 1);
+	#endif
+		if (len != -1) {
+			buf[len] = '\0';
+			return std::string(buf);
+		}
+		exit(EXIT_FAILURE);
+	}
 
-static inline std::string Resource_GetSelfPath(){
-#ifdef WIN32
-	char buf[PATH_MAX];
-	const auto len = GetModuleFileNameA(nullptr, buf, sizeof(buf) - 1);
-#else
-	char buf[PATH_MAX];
-	ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf) - 1);
-#endif
-    if (len != -1) {
-    	buf[len] = '\0';
-    	return std::string(buf);
-    }
-	std::cerr << "ERROR: Failed to get path to self" << std::endl;
-	exit(EXIT_FAILURE);
-}
-
-std::string Resource_GetPath(const char * str){
-	if(str[0] == '/' || str[0] == 'C') return str;
-	std::string rsult = Resource_GetSelfPath();
-	size_t found = rsult.find_last_of("/\\");
-	rsult = rsult.substr(0, found);
-	found = rsult.find_last_of("/\\");
-	rsult = rsult.substr(0, found);
-	rsult += "/data/";
-	rsult += str;
-	std::cout << "Concat str: " << rsult << std::endl;
-	return rsult;
-}
-
+	std::string get(const char * str) {
+		if(str[0] == '/' || str[0] == 'C')
+			return str;
+		
+		std::string rsult = get_exec_path();
+		size_t found = rsult.find_last_of("/\\");
+		rsult = rsult.substr(0, found);
+		found = rsult.find_last_of("/\\");
+		rsult = rsult.substr(0, found);
+		rsult += "/data/";
+		rsult += str;
+		return rsult;
+	}
+};
