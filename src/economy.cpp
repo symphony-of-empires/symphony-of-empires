42af138db76e0986d54f265ed6bbd4c5879fe4ba
superleaf1995 <jesusantonio30122016@gmail.com>
2021-05-16 20:22:55 -0400

    Remove industry function

--- src/economy.cpp
+++ src/economy.cpp
@@ -16,6 +16,17 @@ bool Industry::can_do_output(World * world) {
 	return true;
 }
 
+size_t Industry::get_id(World * world, size_t province_id) {
+	size_t i = 0;
+	for(const auto& industry: world->provinces[province_id]->industries) {
+		if(industry == this) {
+			return i;
+		}
+		i++;
+	}
+	return (size_t)-1;
+}
+
 /// Adds a good by id to a industries stockpile
 void Industry::add_to_stock(World * world, const size_t good_id, const size_t add) {
 	for(size_t i = 0; i < this->stockpile.size(); i++) {
