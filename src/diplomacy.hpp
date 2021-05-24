63b187fc1d847d3e0d6347210533120addebfdb3
superleaf1995 <jesusantonio30122016@gmail.com>
2021-05-24 19:13:07 -0400

    is_foe on diplomacy

--- src/diplomacy.hpp
+++ src/diplomacy.hpp
@@ -5,29 +5,8 @@
 #include "nation.hpp"
 
 namespace Diplomacy {
-	int is_friend(Nation * us, Nation * them) {
-		const size_t idx = g_world->get_nation_id(them);
-		const NationRelation * relation = &us->relations[idx];
-
-		// A high relation means we are friendly <3
-		if(relation->relation >= 50.f) {
-			return true;
-		} else {
-			// Well, but maybe our interest is able to determine our friendliness towards them?
-			if(relation->interest >= relation->relation) {
-				// We cannot be friendly with negative relations
-				if(relation->relation <= 0.f) {
-					return false;
-				}
-
-				// Well, we need to be interested enough to like them
-				if(relation->relation >= relation->interest / relation->relation) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
+	bool is_friend(Nation * us, Nation * them);
+	bool is_foe(Nation * us, Nation * them);
 };
 
 #endif
