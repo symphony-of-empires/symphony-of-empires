4ac829b0d5593c61109d264e27b2fc5b540719f3
superleaf1995 <jesusantonio30122016@gmail.com>
2021-06-10 03:12:03 -0400

    Simple networking (needs serializer work)

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,95 @@
-#include <string>
-#include "world.hpp"
-#include "lua.hpp"
-
-void rendering_main(void);
-
 #include <chrono>
 #include <thread>
-
+#include <iostream>
+#include <fstream>
 #include <atomic>
-std::atomic<bool> run;
-std::atomic<bool> paused;
-std::atomic<bool> do_start;
+#include <cstdlib>
 
 #include <libintl.h>
 #include <locale.h>
 #include "path.hpp"
 #include "network.hpp"
 
-#include <iostream>
-#include <fstream>
-
 #include "io_impl.hpp"
 
+extern TextureManager * g_texture_manager;
 std::mutex world_lock;
+std::atomic<bool> run;
+std::atomic<bool> paused;
+std::atomic<bool> do_start;
+
 int main(int argc, char ** argv) {
 	setlocale(LC_ALL, "");
 	bindtextdomain("main", Path::get("locale").c_str());
 	textdomain("main");
 
-	Client * client;
-	Server * server;
-	World * world = new World();
+	if(!strcmp("server", argv[1])) {
+		Server * server = new Server(1825, 24);
+		World * world = new World(false);
 
-	Archive * test = new Archive();
-	serialize(*test, *world);
-	test->to_file("hello_world.v0");
+		print_info("Size: %zu x %zu", g_world->width, g_world->height);
+		print_info("Seal level: %lu", g_world->sea_level);
+		print_info("Time: %lu", g_world->time);
+		print_info("Cultures: %zu", g_world->cultures.size());
+		print_info("Nations: %zu", g_world->nations.size());
+		print_info("PopTypes: %zu", g_world->pop_types.size());
+		print_info("Provinces: %zu", g_world->provinces.size());
+		print_info("IndustryTypes: %zu", g_world->industry_types.size());
+		print_info("Goods: %zu", g_world->goods.size());
+		print_info("Orders: %zu, Delivers: %zu", g_world->orders.size(), g_world->delivers.size());
 
-#ifndef UNIT_TEST
-	printf("%s\n", gettext("launching rendering thread"));
-	std::thread t1(rendering_main);
+		// TODO: For debugging only, in release should be set via cmd
+		do_start = true;
+		run = true;
+		paused = false;
 
-	while(!do_start);
-	
-	paused = false;
-	while(run) {
-		std::unique_lock<std::mutex> lock(world_lock);
-		world->do_tick();
-		world->client_update();
-		//std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		while(!do_start);
+		while(run) {
+			//std::unique_lock<std::mutex> lock(world_lock);
 
-		while(paused);
+			world_lock.lock();
+			world->do_tick();
+			world_lock.unlock();
+
+			//std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			while(paused);
+		}
+	} else {
+		Client * client = new Client("127.0.0.1", 1825);
+		World * world = new World(true);
+
+		Packet * packet = new Packet(client->get_fd());
+
+		uint16_t version;
+		packet->recv(&version);
+
+		print_info("Server's version: %u\n", version);
+		print_info("Our version: %u\n", 0x0001);
+
+		packet->recv<void>();
+
+		Archive ar = Archive();
+		ar.buffer.resize(packet->size());
+		memcpy(&ar.buffer[0], packet->data(), packet->size());
+		deserialize(ar, *g_world);
+
+		print_info("Size: %zu x %zu", g_world->width, g_world->height);
+		print_info("Seal level: %lu", g_world->sea_level);
+		print_info("Time: %lu", g_world->time);
+		print_info("Cultures: %zu", g_world->cultures.size());
+		print_info("Nations: %zu", g_world->nations.size());
+		print_info("PopTypes: %zu", g_world->pop_types.size());
+		print_info("Provinces: %zu", g_world->provinces.size());
+		print_info("IndustryTypes: %zu", g_world->industry_types.size());
+		print_info("Goods: %zu", g_world->goods.size());
+
+		//g_world = (World *)packet->data();
+
+		while(1) {
+
+		}
+		delete packet;
 	}
-	t1.join();
-
-	//if(!strcmp(argv[1], "client")) {
-	//	client = new Client("127.0.0.1", 4206);
-
-	//uint8_t * arr = new uint8_t[16777216];
-	//std::basic_ofstream<uint8_t> outfile("world_cache.wcyz", std::ofstream::out | std::ofstream::binary);
-	//Serializer<World>::serialize(arr, *world);
-	//outfile.write(arr, 16777216);
-	//delete[] arr;
-	/*} else if(!strcmp(argv[1], "server")) {
-		server = new Server(4206);
-		world = new World();
-	}*/
-#else
-	exit(EXIT_SUCCESS);
-#endif
-
 	return 0;
 }
 
