d3f6cbee99c426982fb2e43e314be96f1c27081c
superleaf1995 <jesusantonio30122016@gmail.com>
2021-07-19 00:04:10 -0400

    Make the game work on MSVC

--- src/print.hpp
+++ src/print.hpp
@@ -3,14 +3,29 @@
 
 #include <sstream>
 #include <stdio.h>
-#define print_error(...)																	\
-	printf("\e[31m%6s: \"%8s\" %i:\e[0m ", __FILE__, __PRETTY_FUNCTION__, __LINE__);		\
-	printf(__VA_ARGS__);															\
+
+/* mscvc does not support this extension of PRETTY_FUNCTION */
+#ifdef _MSC_VER
+#	define print_error(...)																\
+	printf("%6s: %i: ", __FILE__, __LINE__);											\
+	printf(__VA_ARGS__);																\
 	printf("\n");
 
-#define print_info(...)																	\
-	printf("\e[36m%6s: \"%8s\" %i:\e[0m ", __FILE__, __PRETTY_FUNCTION__, __LINE__);		\
-	printf(__VA_ARGS__);															\
+#	define print_info(...)																\
+	printf("%6s: %i: ", __FILE__, __LINE__);											\
+	printf(__VA_ARGS__);																\
 	printf("\n");
+/* mingw and gcc supports PRETTY_FUNCTION */
+#else
+#	define print_error(...)																\
+	printf("\e[31m%6s: \"%8s\" %i:\e[0m ", __FILE__, __PRETTY_FUNCTION__, __LINE__);	\
+	printf(__VA_ARGS__);																\
+	printf("\n");
+
+#	define print_info(...)																\
+	printf("\e[36m%6s: \"%8s\" %i:\e[0m ", __FILE__, __PRETTY_FUNCTION__, __LINE__);	\
+	printf(__VA_ARGS__);																\
+	printf("\n");
+#endif
 
 #endif
