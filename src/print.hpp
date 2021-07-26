c1092eb42255979e6ba2f8b87fda4fbd85db2b28
superleaf1995 <jesusantonio30122016@gmail.com>
2021-07-26 02:10:19 -0400

    Remove useless information from debug print statements

--- src/print.hpp
+++ src/print.hpp
@@ -7,22 +7,20 @@
 /* mscvc does not support this extension of PRETTY_FUNCTION */
 #ifdef _MSC_VER
 #	define print_error(...)																\
-    printf("%6s: %i: ", __FILE__, __LINE__);											\
     printf(__VA_ARGS__);																\
     printf("\n");
 #	define print_info(...)																\
-    printf("%6s: %i: ", __FILE__, __LINE__);											\
     printf(__VA_ARGS__);																\
     printf("\n");
 /* mingw and gcc supports PRETTY_FUNCTION */
 #else
 #	define print_error(...)																\
-    printf("\e[31m%6s: \"%8s\" %i:\e[0m ", __FILE__, __PRETTY_FUNCTION__, __LINE__);	\
+    printf("\e[31m[ERROR]\e[0m ");	                                                    \
     printf(__VA_ARGS__);																\
     printf("\n");
 
 #	define print_info(...)																\
-    printf("\e[36m%6s: \"%8s\" %i:\e[0m ", __FILE__, __PRETTY_FUNCTION__, __LINE__);	\
+    printf("\e[36m[INFO]\e[0m ");	                                                    \
     printf(__VA_ARGS__);																\
     printf("\n");
 #endif
