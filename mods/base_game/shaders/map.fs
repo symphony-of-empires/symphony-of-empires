db96339b4ec952e92db4337400a0ac0b9b6fa9f3
Erik Jonasson <Erik Jonasson>
2021-08-07 16:48:04 +0200

    Shader code improved

--- mods/base_game/shaders/map.fs
+++ mods/base_game/shaders/map.fs
@@ -2,10 +2,17 @@
 out vec4 FragColor;
   
 in vec2 TexCoord;
+in vec3 Color;
 
-uniform sampler2D ourTexture;
+uniform sampler2D terrain_texture;
+uniform sampler2D terrain_sheet;
 
 void main()
 {
-    FragColor = texture(ourTexture, TexCoord);
+    vec4 land = vec4(0., 0.7, 0., 1.);
+    vec4 water = vec4(0., 0., 0.7, 1.);
+    vec3 coord = texture(terrain_texture, TexCoord).rgb;
+    vec4 ground = mix(water, land, step(0.0001, coord.b));
+    vec4 provColor = texture(terrain_sheet, coord.rg);
+    FragColor = mix(ground, provColor, provColor.a);
 }
--- mods/base_game/shaders/map.vs
+++ mods/base_game/shaders/map.vs
@@ -1,13 +1,16 @@
 #version 330 compatibility
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
+layout (location = 2) in vec3 aColor;
 
-uniform mat4 viewProj;
+uniform mat4 view_proj;
 
 out vec2 TexCoord;
+out vec3 Color;
 
 void main()
 {
-    gl_Position = viewProj * vec4(aPos, 1.0);
+    gl_Position = view_proj * vec4(aPos, 1.0);
     TexCoord = aTexCoord;
+    Color = aColor;
 }
