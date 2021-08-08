c9f2783266a80c33dfcb8f389802dfc6580f5513
superleaf1995 <jesusantonio30122016@gmail.com>
2021-08-07 22:48:56 -0400

    Increment server delay

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char** argv) {
             }
             future = std::async(std::launch::async, async_get_input);
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 #endif
         
