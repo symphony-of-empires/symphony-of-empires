68d7d47c82e7d99b9ac92ff407c330ad18305a6c
Erik Jonasson <erikjonassonn@outlook.com>
2021-08-23 22:22:31 +0200

    Shader fix

--- mods/base_game/shaders/simple_model.fs
+++ mods/base_game/shaders/simple_model.fs
@@ -5,9 +5,9 @@ out vec4 frag_colour;
 in vec2 v_texcoord;
 in vec3 v_colour;
 
-uniform sampler2D texture;
+uniform sampler2D tex;
 
 void main() {
-    frag_colour = texture(texture, v_texcoord);
+    frag_colour = texture(tex, v_texcoord);
     //frag_colour = vec4(1.0, 1.0, 1.0, 1.0);
 }
