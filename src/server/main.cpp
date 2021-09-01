eb09913a452244ac81e312bb3956e4779df6b5f2
jesus <jesusantonio30122016@gmail.com>
2021-09-01 15:39:18 -0500

    Fix for paths not found

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -47,8 +47,8 @@ int main(int argc, char** argv) {
     bindtextdomain("main", Path::get("locale").c_str());
     textdomain("main");
 #endif
-    Path::add_path("test");
-    //Path::add_path("base_game");
+    //Path::add_path("test");
+    Path::add_path("base_game");
 
 #ifndef UNIT_TEST
     // Run as a server for servicing multiple clients
