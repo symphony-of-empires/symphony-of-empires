417cb8a920f4f48ff095be331733affc811a5500
jesus <jesusantonio30122016@gmail.com>
2021-09-01 02:21:16 -0500

    Add UI and window resizing and fix bug where NEXT and PREVIOUS buttons wont work

--- src/client/interface/top_window.cpp
+++ src/client/interface/top_window.cpp
@@ -19,14 +19,20 @@ TopWindow::TopWindow(GameState& _gs) : gs{_gs} {
     gdp_chart->current_texture = &top_win_chart_tex;
     gdp_chart->data.clear();
     gdp_chart->user_data = (void*)this;
-    gdp_chart->on_click = (UI::Callback)show_industry_cb;
+    gdp_chart->on_click = (UI::Callback)([](UI::Widget& w, void* data) {
+        TopWindow* d = (TopWindow*)data;
+        d->gs.industry_view_nation->show();
+    });
 
     pop_chart = new UI::Chart(504, 120, top_win_chart_tex.width, top_win_chart_tex.height, top_win);
     pop_chart->text("Population");
     pop_chart->current_texture = &top_win_chart_tex;
     pop_chart->data.clear();
     pop_chart->user_data = (void*)this;
-    pop_chart->on_click = (UI::Callback)show_pops_cb;
+    pop_chart->on_click = (UI::Callback)([](UI::Widget& w, void* data) {
+        TopWindow* d = (TopWindow*)data;
+        d->gs.pop_view_nation->show();
+    });
 
     hdi_chart = new UI::Chart(504, 197, top_win_chart_tex.width, top_win_chart_tex.height, top_win);
     hdi_chart->text("HDI");
@@ -35,41 +41,44 @@ TopWindow::TopWindow(GameState& _gs) : gs{_gs} {
 
     extern void ui_reform_policies(UI::Widget&, void*);
     hdi_chart->user_data = (void*)this;
-    hdi_chart->on_click = (UI::Callback)show_reforms_cb;
+    hdi_chart->on_click = (UI::Callback)([](UI::Widget& w, void* data) {
+        TopWindow* d = (TopWindow*)data;
+        d->gs.ui_reform->show();
+    });
 
     UI::Image* current_flag = new UI::Image(9, 43, 188, 87, gs.map->nation_flags[g_world->get_id(gs.curr_nation)], top_win);
 
     money_icon = new UI::Image(209, 43 + (28 * 0), icon_money_tex.width, icon_money_tex.height, &icon_money_tex, top_win);
     money_lab = new UI::Label(0, 43 + (28 * 0), "?", top_win);
-    money_lab->right_side_of((*money_icon));
+    money_lab->right_side_of(*money_icon);
 
     prestige_icon = new UI::Image(209, 43 + (28 * 1), icon_prestige_tex.width, icon_prestige_tex.height, &icon_prestige_tex, top_win);
     prestige_lab = new UI::Label(0, 43 + (28 * 1), "?", top_win);
-    prestige_lab->right_side_of((*prestige_icon));
+    prestige_lab->right_side_of(*prestige_icon);
 
     economy_icon = new UI::Image(209, 43 + (28 * 2), icon_economy_score_tex.width, icon_economy_score_tex.height, &icon_economy_score_tex, top_win);
     economy_lab = new UI::Label(0, 43 + (28 * 2), "?", top_win);
-    economy_lab->right_side_of((*economy_icon));
+    economy_lab->right_side_of(*economy_icon);
 
     big_brain_icon = new UI::Image(209, 43 + (28 * 3), icon_big_brain_tex.width, icon_big_brain_tex.height, &icon_big_brain_tex, top_win);
     big_brain_lab = new UI::Label(0, 43 + (28 * 3), "?", top_win);
-    big_brain_lab->right_side_of((*big_brain_icon));
+    big_brain_lab->right_side_of(*big_brain_icon);
 
     militancy_icon = new UI::Image(209, 43 + (28 * 4), icon_militancy_tex.width, icon_militancy_tex.height, &icon_militancy_tex, top_win);
     militancy_lab = new UI::Label(0, 43 + (28 * 4), "?", top_win);
-    militancy_lab->right_side_of((*militancy_icon));
+    militancy_lab->right_side_of(*militancy_icon);
 
     population_icon = new UI::Image(209, 43 + (28 * 5), icon_population_tex.width, icon_population_tex.height, &icon_population_tex, top_win);
     population_lab = new UI::Label(0, 43 + (28 * 5), "?", top_win);
-    population_lab->right_side_of((*population_icon));
+    population_lab->right_side_of(*population_icon);
 }
 
 void TopWindow::show_industry_cb(UI::Widget& w, TopWindow* data) {
-    data->gs.industry_view_nation->show();
+    
 }
 void TopWindow::show_pops_cb(UI::Widget& w, TopWindow* data) {
-    data->gs.pop_view_nation->show();
+    
 }
 void TopWindow::show_reforms_cb(UI::Widget& w, TopWindow* data) {
-    data->gs.ui_reform->show();
+    
 }
