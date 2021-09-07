43c5dff3409ae3d625e8152561205d2f463c61bd
jesus <jesusantonio30122016@gmail.com>
2021-09-06 22:15:47 -0500

    STBI can do RGBA conversion on the fly

--- src/binary_image.cpp
+++ src/binary_image.cpp
@@ -28,37 +28,15 @@ BinaryImage::BinaryImage(const BinaryImage& tex) {
 
 void BinaryImage::from_file(const std::string& path) {
     int i_width, i_height, i_channels;
-    buffer = (uint32_t *)stbi_load(Path::get(path).c_str(), &i_width, &i_height, &i_channels, 0);
+
+    // stbi can do the conversion to RGBA for us ;)
+    buffer = (uint32_t *)stbi_load(Path::get(path).c_str(), &i_width, &i_height, &i_channels, 4);
     if(buffer == NULL) {
         throw BinaryImageException(path, "Image load error");
     }
 
     width = (size_t)i_width;
     height = (size_t)i_height;
-
-    if(i_channels != 4) {
-        if(i_channels < 3) {
-            throw BinaryImageException(path, "Image must be RGB");
-        }
-
-        // Extend the image to RGBA
-        print_info("Extending non-RGBA image to RGBA");
-        uint32_t *second_buffer = new uint32_t[width * height];
-        if(second_buffer == NULL) {
-            throw BinaryImageException(path, "Out of memory");
-        }
-
-        uint8_t *c_buffer = (uint8_t *)buffer;
-        for(size_t i = 0, j = 0; i < (width * height) * i_channels && j < (width * height); i += i_channels, j++) {
-            uint32_t colour = (uint32_t)c_buffer[i + 0]
-                | ((uint32_t)c_buffer[i + 1] << 8)
-                | ((uint32_t)c_buffer[i + 2] << 16)
-                | ((uint32_t)0xff << 24);
-            second_buffer[j] = colour;
-        }
-        free(buffer);
-        buffer = second_buffer;
-    }
 }
 
 BinaryImage::~BinaryImage() {
