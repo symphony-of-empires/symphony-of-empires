54890c62bbc338ec1c39dab74278f02d0bb8d1d8
jesus <jesusantonio30122016@gmail.com>
2021-09-21 09:19:43 -0500

    Remove useless code from the Lua API and add inventions

--- src/server/lua_api.hpp
+++ src/server/lua_api.hpp
@@ -29,6 +29,9 @@ namespace LuaAPI {
 
     int register_new_table(lua_State* L, const std::string& name, const luaL_Reg meta[], const luaL_Reg methods[]);
 
+    int add_invention(lua_State* L);
+    int get_invention(lua_State* L);
+
     int add_technology(lua_State* L);
     int get_technology(lua_State* L);
     int add_req_tech_to_tech(lua_State* L);
