1df4ef278fd39b75345267d13f59eee69a96ae5c
jesus <jesusantonio30122016@gmail.com>
2021-09-28 12:14:47 -0500

    Province view economic data

--- src/client/interface/province_view.cpp
+++ src/client/interface/province_view.cpp
@@ -14,42 +14,68 @@ void ProvinceView::colonize_province_cb(UI::Widget& w, ProvinceView* data) {
     // data->gs.curr_nation->budget -= 10000;
 }
 
-ProvinceView::ProvinceView(GameState& _gs, UI::Window* top_win, const Tile& tile) : gs{_gs}, provinceId{tile.province_id} {
-    World* world = gs.world;
+ProvinceView::ProvinceView(GameState& _gs, const Tile& tile)
+    : gs{_gs},
+      UI::Window(0, 0, 320, 400) {
+    
+    World& world = *gs.world;
 
-    UI::Window* province_view_win = new UI::Window(0, 0, 320, 425);
-    province_view_win->text("province overview");
-    province_view_win->below_of(*top_win);
-
-    selected_province = world->provinces[tile.province_id];
+    province = world.provinces[tile.province_id];
+    text(province->name.c_str());
 
     const UnifiedRender::Texture& province_view_decor = g_texture_manager->load_texture(Path::get("ui/province_view_terrain.png"));
-    UI::Image* view_province_decor = new UI::Image(9, 43, province_view_decor.width, province_view_decor.height, &province_view_decor, province_view_win);
-    view_province_decor->text(world->provinces[tile.province_id]->name.c_str());
+    UI::Image* view_province_decor = new UI::Image(9, 43, province_view_decor.width, province_view_decor.height, &province_view_decor, this);
+    view_province_decor->text(province->name.c_str());
 
-    UI::Button* view_province_pops = new UI::Button(9, 193, 303, 38, province_view_win);
+    UI::Button* view_province_pops = new UI::Button(9, 193, 303, 38, this);
     view_province_pops->text("Population");
-    view_province_pops->user_data = (void*)world->provinces[tile.province_id];
+    view_province_pops->user_data = (void*)province;
 
-    UI::Button* view_province_ind = new UI::Button(9, 0, 303, 38, province_view_win);
+    UI::Button* view_province_ind = new UI::Button(9, 0, 303, 38, this);
     view_province_ind->text("Economic activity");
     view_province_ind->below_of(*view_province_pops);
-    view_province_ind->user_data = (void*)world->provinces[tile.province_id];
+    view_province_ind->user_data = (void*)this;
+    view_province_ind->on_click = (UI::Callback)([](UI::Widget& w, void *data) {
+        ProvinceView *state = (ProvinceView*)data;
+        new ProvinceViewEconomic(state->gs, state->province);
+    });
 
-    UI::Button* view_province_owner = new UI::Button(9, 0, 303, 38, province_view_win);
+    UI::Button* view_province_owner = new UI::Button(9, 0, 303, 38, this);
     view_province_owner->text("Owner info");
     view_province_owner->below_of(*view_province_ind);
-    view_province_owner->user_data = (void*)world->provinces[tile.province_id];
+    view_province_owner->user_data = (void*)province;
 
-    UI::CloseButton* ok_btn = new UI::CloseButton(9, 0, 303, 38, province_view_win);
+    UI::CloseButton* ok_btn = new UI::CloseButton(9, 0, 303, 38, this);
     ok_btn->text("ok");
     ok_btn->below_of(*view_province_owner);
 
-    if (world->provinces[tile.province_id]->owner == nullptr) {
-        UI::Button* colonize_province_btn = new UI::Button(9, 0, 303, 38, province_view_win);
+    if (world.provinces[tile.province_id]->owner == nullptr) {
+        UI::Button* colonize_province_btn = new UI::Button(9, 0, 303, 38, this);
         colonize_province_btn->text("Colonize");
         colonize_province_btn->below_of(*ok_btn);
         colonize_province_btn->user_data = this;
         colonize_province_btn->on_click = (UI::Callback)colonize_province_cb;
     }
+}
+
+ProvinceViewEconomic::ProvinceViewEconomic(GameState& _gs, Province* _province)
+    : gs{_gs},
+      province{_province},
+      UI::Window(0, 0, 320, 400) {
+    
+    UI::CloseButton* ok_btn = new UI::CloseButton(9, 24, 303, 38, this);
+    ok_btn->text("ok");
+
+    UI::Chart* pop_chart = new UI::Chart(0, 32, 120, 64, this);
+    pop_chart->below_of(*ok_btn);
+    pop_chart->user_data = this;
+    pop_chart->on_update = (UI::Callback)([](UI::Widget& w, void* data) {
+        ProvinceViewEconomic *state = (ProvinceViewEconomic*)data;
+        UI::Chart& wc = dynamic_cast<UI::Chart&>(w);
+        if (state->gs.world->time % 48 == 35) {
+            wc.data.push_back(state->province->total_pops());
+            if (wc.data.size() >= 30)
+                wc.data.pop_front();
+        }
+    });
 }
