4c1edeb5c9fe11ab6d1701df4f59f272b217b8be
jesus <jesusantonio30122016@gmail.com>
2021-10-05 13:28:46 -0500

    Add provinces from the portugalicia mod into the base game

---
+++ mods/ie_core/scripts/provinces/europe/portugal.lua
@@ -0,0 +1,122 @@
+--------------------------------------------------------------
+-- Continent: Europe
+-- Subcontinent: Iberia
+-- Country: Portugal
+--Porto
+province = Province:new{ ref_name = "porto", color = 0xae4da1 }
+province.name = _("Porto")
+Province:register(province)
+Province:add_pop(province, artisan, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, farmer, portuguese, catholic, 40000, 0.8)
+Province:add_pop(province, soldier, portuguese, catholic, 8000, 0.8)
+Province:add_pop(province, craftsmen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, bureaucrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, aristocrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, clergymen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, laborer, portuguese, catholic, 50000, 0.8)
+Province:add_pop(province, entrepreneur, portuguese, catholic, 800, 0.9)
+Province:add_nucleus(province, portugal)
+Province:add_nucleus(province, iberia)
+Province:give_to(province, portugal)
+
+--Villa Real
+province = Province:new{ ref_name = "villa_real", color = 0xae7ea1 }
+province.name = _("Villa Real")
+Province:register(province)
+Province:add_pop(province, artisan, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, farmer, portuguese, catholic, 40000, 0.8)
+Province:add_pop(province, soldier, portuguese, catholic, 8000, 0.8)
+Province:add_pop(province, craftsmen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, bureaucrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, aristocrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, clergymen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, laborer, portuguese, catholic, 50000, 0.8)
+Province:add_pop(province, entrepreneur, portuguese, catholic, 800, 0.9)
+Province:add_nucleus(province, portugal)
+Province:add_nucleus(province, iberia)
+Province:give_to(province, portugal)
+
+--Aveiro
+province = Province:new{ ref_name = "aveiro", color = 0x9d7a79 }
+province.name = _("Aveiro")
+Province:register(province)
+Province:add_pop(province, artisan, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, farmer, portuguese, catholic, 40000, 0.8)
+Province:add_pop(province, soldier, portuguese, catholic, 8000, 0.8)
+Province:add_pop(province, craftsmen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, bureaucrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, aristocrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, clergymen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, laborer, portuguese, catholic, 50000, 0.8)
+Province:add_pop(province, entrepreneur, portuguese, catholic, 800, 0.9)
+Province:add_nucleus(province, portugal)
+Province:add_nucleus(province, iberia)
+Province:give_to(province, portugal)
+
+--Guarda
+province = Province:new{ ref_name = "guarda", color = 0x9d6b9f }
+province.name = _("Guarda")
+Province:register(province)
+Province:add_pop(province, artisan, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, farmer, portuguese, catholic, 40000, 0.8)
+Province:add_pop(province, soldier, portuguese, catholic, 8000, 0.8)
+Province:add_pop(province, craftsmen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, bureaucrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, aristocrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, clergymen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, laborer, portuguese, catholic, 50000, 0.8)
+Province:add_pop(province, entrepreneur, portuguese, catholic, 800, 0.9)
+Province:add_nucleus(province, portugal)
+Province:add_nucleus(province, iberia)
+Province:give_to(province, portugal)
+
+--Lisbon
+province = Province:new{ ref_name = "lisbon", color = 0x86549f }
+province.name = _("Lisbon")
+Province:register(province)
+Province:add_pop(province, artisan, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, farmer, portuguese, catholic, 40000, 0.8)
+Province:add_pop(province, soldier, portuguese, catholic, 8000, 0.8)
+Province:add_pop(province, craftsmen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, bureaucrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, aristocrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, clergymen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, laborer, portuguese, catholic, 50000, 0.8)
+Province:add_pop(province, entrepreneur, portuguese, catholic, 800, 0.9)
+Province:add_nucleus(province, portugal)
+Province:add_nucleus(province, iberia)
+Province:give_to(province, portugal)
+
+--Évora
+province = Province:new{ ref_name = "evora", color = 0x9d6c90 }
+province.name = _("Évora")
+Province:register(province)
+Province:add_pop(province, artisan, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, farmer, portuguese, catholic, 40000, 0.8)
+Province:add_pop(province, soldier, portuguese, catholic, 8000, 0.8)
+Province:add_pop(province, craftsmen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, bureaucrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, aristocrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, clergymen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, laborer, portuguese, catholic, 50000, 0.8)
+Province:add_pop(province, entrepreneur, portuguese, catholic, 800, 0.9)
+Province:add_nucleus(province, portugal)
+Province:add_nucleus(province, iberia)
+Province:give_to(province, portugal)
+
+--Faro
+province = Province:new{ ref_name = "faro", color = 0x9d3c90 }
+province.name = _("faro")
+Province:register(province)
+Province:add_pop(province, artisan, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, farmer, portuguese, catholic, 40000, 0.8)
+Province:add_pop(province, soldier, portuguese, catholic, 8000, 0.8)
+Province:add_pop(province, craftsmen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, bureaucrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, aristocrat, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, clergymen, portuguese, catholic, 5000, 0.9)
+Province:add_pop(province, laborer, portuguese, catholic, 50000, 0.8)
+Province:add_pop(province, entrepreneur, portuguese, catholic, 800, 0.9)
+Province:add_nucleus(province, portugal)
+Province:add_nucleus(province, iberia)
+Province:give_to(province, portugal)
--- mods/ie_core/scripts/religions.lua
+++ mods/ie_core/scripts/religions.lua
@@ -2,18 +2,23 @@
 protestant = Religion:new{ ref_name = "protestant" }
 protestant.name = _("Protestant")
 Religion:register(protestant)
+
 catholic = Religion:new{ ref_name = "catholic" }
 catholic.name = _("Catholic")
 Religion:register(catholic)
+
 orthodox = Religion:new{ ref_name = "orthodox" }
 orthodox.name = _("Orthodox")
 Religion:register(orthodox)
+
 islamic = Religion:new{ ref_name = "islamic" }
 islamic.name = _("Islamic")
 Religion:register(islamic)
+
 buddhist = Religion:new{ ref_name = "buddhist" }
 buddhist.name = _("Buddhist")
 Religion:register(buddhist)
+
 animist = Religion:new{ ref_name = "animist" }
 animist.name = _("Animist")
 Religion:register(animist)
