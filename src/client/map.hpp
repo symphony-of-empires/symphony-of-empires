df64213c9561a969ddc9e1232d758ba3c3430820
Erik Jonasson <erikjonassonn@outlook.com>
2021-10-19 00:01:19 +0200

    Code for map modes

--- src/client/map.hpp
+++ src/client/map.hpp
@@ -33,17 +33,18 @@ public:
     float wind_osc = 0.f;
 
     const World& world;
-    
+
     UnifiedRender::Texture* div_topo_tex;
     UnifiedRender::Texture* div_sheet_tex;
     const UnifiedRender::Texture* water_tex;
     const UnifiedRender::Texture* noise_tex;
     const UnifiedRender::Texture* topo_tex;
     const UnifiedRender::Texture* terrain_tex;
+    const UnifiedRender::Texture* border_tex;
     // const UnifiedRender::Texture* terrain_sheet;
     UnifiedRender::TextureArray* terrain_sheet;
     UnifiedRender::OpenGl::PrimitiveSquare* map_quad;
-    UnifiedRender::OpenGl::Program* map_shader,* obj_shader;
+    UnifiedRender::OpenGl::Program* map_shader, * obj_shader, * border_shader;
     const UnifiedRender::Texture* overlay_tex;
     GLuint coastline_gl_list;
     GLuint frame_buffer;
@@ -52,6 +53,8 @@ public:
     void draw_flag(const Nation* nation);
     void draw(Camera& cam, const int width, const int height);
     void handle_click(GameState& gs, SDL_Event event);
+    void set_map_mode(std::vector<std::pair<Province::Id, uint32_t>> province_colors);
+    void update_borders();
 };
 
 #endif
