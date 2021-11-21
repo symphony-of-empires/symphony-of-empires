689697bbdb1e6a6b3c3ebb5d014340ffd960ef80
superleaf1995 <jesusantonio30122016@gmail.com>
2021-11-21 00:50:13 -0600

    Language utilities for creating good-sounding sentences + new api functions

--- mods/ie_core/scripts/nations/africa.lua
+++ mods/ie_core/scripts/nations/africa.lua
@@ -3,12 +3,14 @@
 -- Initial nations
 tunis = Nation:new{ ref_name = "tunis" }
 tunis.name = _("Tunis")
+tunis.adjective = _("Tunisian")
 tunis:register()
 tunis:add_accepted_culture(arabic)
 tunis:add_client_hint(communism, _("Tunis"), 0x808000)
 
 algeirs = Nation:new{ ref_name = "algeirs" }
 algeirs.name = _("Algeirs")
+algeirs.adjective = _("Algerian")
 algeirs:register()
 algeirs:add_accepted_culture(arabic)
 algeirs:add_client_hint(communism, _("Algeirs"), 0x808000)
@@ -20,11 +22,13 @@ abbyssina:add_client_hint(communism, _("Abbyssina"), 0x808000)
 
 egypt = Nation:new{ ref_name = "egypt" }
 egypt.name = _("Egypt")
+egypt.adjective = _("Egyptians")
 egypt:register()
 egypt:add_client_hint(communism, _("Egypt"), 0x808000)
 
 tripolitania = Nation:new{ ref_name = "tripolitania" }
 tripolitania.name = _("Tripolitania")
+tripolitania.adjective = _("Tripolitanians")
 tripolitania:register()
 tripolitania:add_client_hint(communism, _("Tripolitania"), 0x808000)
 
@@ -35,6 +39,8 @@ ashantee:add_client_hint(communism, _("Ashantee"), 0x808000)
 -- Formable nations
 arabia = Nation:new{ ref_name = "arabia" }
 arabia.name = _("Arabia")
+arabia.adjective = "Arabic"
+arabia.noun = "Arab"
 arabia:register()
 arabia:add_accepted_culture(arabic)
 arabia:add_client_hint(communism, _("Arabia"), 0x808000)
