36753a7f3d0bad5dc5398ba47f11d46fb3de81ed
superleaf1995 <jesusantonio30122016@gmail.com>
2021-12-10 18:23:33 -0600

    AI controlling different stuff

--- src/server/server_network.cpp
+++ src/server/server_network.cpp
@@ -459,7 +459,14 @@ void Server::net_loop(int id) {
                         if(nation == nullptr)
                             throw ServerException("Unknown nation");
                         selected_nation = nation;
-                        selected_nation->is_ai = false;
+                        selected_nation->ai_do_policies = false;
+                        selected_nation->ai_do_research = false;
+                        selected_nation->ai_do_diplomacy = false;
+                        selected_nation->ai_do_cmd_troops = false;
+                        selected_nation->ai_do_unit_production = false;
+                        selected_nation->ai_do_build_production = false;
+                        selected_nation->ai_handle_treaties = false;
+                        selected_nation->ai_handle_events = false;
                         print_info("Nation [%s] selected by client %zu", selected_nation->ref_name.c_str(), (size_t)id);
                     } break;
                     case ActionType::DIPLO_INC_RELATIONS: {
