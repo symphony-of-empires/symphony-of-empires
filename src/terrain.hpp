fbf219ef2bdc83883764921abcfe2cb013610f2f
superleaf1995 <jesusantonio30122016@gmail.com>
2021-12-15 00:18:57 -0600

    Terrain types given to provinces

--- src/terrain.hpp
+++ src/terrain.hpp
@@ -8,6 +8,9 @@ class TerrainType : public RefnameEntity<uint8_t>  {
 public:
     // Associative colour (with bitmap)
     uint32_t color;
+
+    // Should this terrain be treated as sea?
+    bool is_water_body;
     
     float movement_penalty;
 };
