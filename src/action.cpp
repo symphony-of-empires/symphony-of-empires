7e79cdf6a5176ed0e1568978164790e6915de2d8
superleaf1995 <jesusantonio30122016@gmail.com>
2021-12-18 01:36:31 -0600

    Declare war and stuff

--- src/action.cpp
+++ src/action.cpp
@@ -12,6 +12,36 @@
 
 using namespace Action;
 
+Packet DiploIncRelations::form_packet(Nation* target) {
+    Packet packet = Packet();
+    Archive ar = Archive();
+    ActionType action = ActionType::DIPLO_INC_RELATIONS;
+    ::serialize(ar, &action);
+    ::serialize(ar, &target);
+    packet.data(ar.get_buffer(), ar.size());
+    return packet;
+}
+
+Packet DiploDecRelations::form_packet(Nation* target) {
+    Packet packet = Packet();
+    Archive ar = Archive();
+    ActionType action = ActionType::DIPLO_DEC_RELATIONS;
+    ::serialize(ar, &action);
+    ::serialize(ar, &target);
+    packet.data(ar.get_buffer(), ar.size());
+    return packet;
+}
+
+Packet DiploDeclareWar::form_packet(Nation* target) {
+    Packet packet = Packet();
+    Archive ar = Archive();
+    ActionType action = ActionType::DIPLO_DECLARE_WAR;
+    ::serialize(ar, &action);
+    ::serialize(ar, &target);
+    packet.data(ar.get_buffer(), ar.size());
+    return packet;
+}
+
 Packet ProductUpdate::form_packet(const std::vector<Product*>& list) {
     Packet packet = Packet();
     Archive ar = Archive();
