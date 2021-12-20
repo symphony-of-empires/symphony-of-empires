f23b5c5941520ac9a5cb11d86eb11beb3af14c4c
superleaf1995 <jesusantonio30122016@gmail.com>
2021-12-20 16:16:33 -0600

    Linearize map_div loading

--- src/client/color.cpp
+++ src/client/color.cpp
@@ -4,18 +4,15 @@
 
 using namespace UI;
 
-Color Color::rgba8(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
-{
+Color Color::rgba8(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) {
     return Color(red / 256.f, green / 256.f, blue / 256.f, alpha / 256.f);
 }
 
-Color Color::rgb8(uint8_t red, uint8_t green, uint8_t blue)
-{
+Color Color::rgb8(uint8_t red, uint8_t green, uint8_t blue) {
     return Color(red / 256.f, green / 256.f, blue / 256.f);
 }
 
-Color Color::rgba32(uint32_t argb)
-{
+Color Color::rgba32(uint32_t argb) {
     float a = ((argb >> 24) & 0xff) / 256.f;
     float b = ((argb >> 16) & 0xff) / 256.f;
     float g = ((argb >> 8) & 0xff) / 256.f;
@@ -31,6 +28,7 @@ uint32_t Color::get_value() const {
     uint32_t color = (alpha << 24) | (blue << 16) | (green << 8) | (red);
     return color;
 }
+
 Color Color::lerp(Color color1, Color color2, float lamda) {
     float r = color1.r * (1 - lamda) + color2.r * lamda;
     float g = color1.g * (1 - lamda) + color2.g * lamda;
