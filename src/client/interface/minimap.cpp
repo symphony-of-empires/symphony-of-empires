3f6f61b219e3ecdb8416047a81c288cac5be758d
coderguy57 <erikjonassonn@outlook.com>
2021-12-20 21:24:30 +0100

    More map modes

--- src/client/interface/minimap.cpp
+++ src/client/interface/minimap.cpp
@@ -3,11 +3,15 @@
 #include "world.hpp"
 #include "path.hpp"
 #include "unified_render/texture.hpp"
+#include <unordered_map>
 
 using namespace Interface;
 
 std::vector<ProvinceColor> terrain_map_mode(const World& world);
+std::vector<ProvinceColor> terrain_color_map_mode(const World& world);
 std::vector<ProvinceColor> population_map_mode(const World& world);
+std::vector<ProvinceColor> culture_map_mode(const World& world);
+std::vector<ProvinceColor> religion_map_mode(const World& world);
 
 Minimap::Minimap(GameState& _gs, int x, int y, UI_Origin origin)
     : gs{ _gs },
@@ -49,6 +53,33 @@ Minimap::Minimap(GameState& _gs, int x, int y, UI_Origin origin)
         mapmode_generator map_mode = population_map_mode;
         state->gs.map->set_map_mode(map_mode);
     });
+    UI::Button* terrain_color_button = new UI::Button(110, 160, 100, 24, this);
+    terrain_color_button->user_data = this;
+    terrain_color_button->text("Terrain type");
+    terrain_color_button->on_click = ([](UI::Widget& w, void* data) {
+        Minimap* state = (Minimap*)data;
+
+        mapmode_generator map_mode = terrain_color_map_mode;
+        state->gs.map->set_map_mode(map_mode);
+    });
+    UI::Button* culture_button = new UI::Button(110, 200, 100, 24, this);
+    culture_button->user_data = this;
+    culture_button->text("Culture");
+    culture_button->on_click = ([](UI::Widget& w, void* data) {
+        Minimap* state = (Minimap*)data;
+
+        mapmode_generator map_mode = culture_map_mode;
+        state->gs.map->set_map_mode(map_mode);
+    });
+    UI::Button* religion_button = new UI::Button(110, 240, 100, 24, this);
+    religion_button->user_data = this;
+    religion_button->text("Religion");
+    religion_button->on_click = ([](UI::Widget& w, void* data) {
+        Minimap* state = (Minimap*)data;
+
+        mapmode_generator map_mode = religion_map_mode;
+        state->gs.map->set_map_mode(map_mode);
+    });
 }
 
 std::vector<ProvinceColor> terrain_map_mode(const World& world) {
@@ -63,9 +94,24 @@ std::vector<ProvinceColor> terrain_map_mode(const World& world) {
     return province_color;
 }
 
+std::vector<ProvinceColor> terrain_color_map_mode(const World& world) {
+    std::vector<ProvinceColor> province_color;
+    for(unsigned int i = 0; i < world.provinces.size(); i++) {
+        Province* province = world.provinces[i];
+        UI::Color color = UI::Color::rgba32(province->terrain_type->color);
+        province_color.push_back(ProvinceColor(i, color));
+    }
+    // Water
+    province_color.push_back(ProvinceColor((Province::Id)-2, UI::Color::rgba32(0x00000000)));
+    // Land
+    province_color.push_back(ProvinceColor((Province::Id)-1, UI::Color::rgba32(0x00000000)));
+    return province_color;
+}
+
 std::vector<ProvinceColor> population_map_mode(const World& world) {
+    // Find the maximum amount of pops in one province
     std::vector<std::pair<Province::Id, uint32_t>> province_amounts;
-    uint32_t max_amount = 0;
+    uint32_t max_amount = 1;
     for(auto const& province : world.provinces) {
         uint32_t color;
         uint32_t amount = 0;
@@ -75,7 +121,8 @@ std::vector<ProvinceColor> population_map_mode(const World& world) {
         max_amount = std::max<size_t>(amount, max_amount);
         province_amounts.push_back(std::make_pair(world.get_id(province), amount));
     }
-    max_amount = std::max<uint32_t>(1, max_amount);
+
+    // Mix each color depending of how many live there compared to max_amount
     UI::Color max = UI::Color::rgb8(255, 229, 217);
     UI::Color min = UI::Color::rgb8(220, 46, 35);
     std::vector<ProvinceColor> province_color;
@@ -86,5 +133,79 @@ std::vector<ProvinceColor> population_map_mode(const World& world) {
         UI::Color color = UI::Color::lerp(min, max, ratio);
         province_color.push_back(ProvinceColor(prov_id, color));
     }
+    return province_color;
+}
+
+std::vector<ProvinceColor> culture_map_mode(const World& world) {
+    std::vector<ProvinceColor> province_color;
+    UI::Color min = UI::Color::rgb8(255, 255, 255);
+    for(unsigned int i = 0; i < world.provinces.size(); i++) {
+        Province* province = world.provinces[i];
+        std::unordered_map<Culture::Id, size_t> culture_amounts;
+        size_t total_amount = 0;
+        size_t max_amount = 0;
+        Culture::Id max_culture_id = 0;
+        for(unsigned int j = 0; j < province->pops.size(); j++) {
+            const Pop& pop = province->pops[j];
+            total_amount += pop.size;
+
+            auto search = culture_amounts.find(pop.culture->cached_id);
+            if(search == culture_amounts.end()) {
+                culture_amounts[pop.culture->cached_id] = pop.size;
+            }
+            else {
+                culture_amounts[pop.culture->cached_id] += pop.size;
+            }
+            size_t amount = culture_amounts[pop.culture->cached_id];
+            if(amount > max_amount) {
+                max_amount = amount;
+                max_culture_id = pop.culture->cached_id;
+            }
+        }
+        UI::Color max = UI::Color::rgba32(world.cultures[max_culture_id]->color);
+        UI::Color color = UI::Color::lerp(min, max, max_amount / total_amount);
+        province_color.push_back(ProvinceColor(i, color));
+    }
+    // Water
+    province_color.push_back(ProvinceColor((Province::Id)-2, UI::Color::rgba32(0x00000000)));
+    // Land
+    province_color.push_back(ProvinceColor((Province::Id)-1, UI::Color::rgba32(0xffdddddd)));
+    return province_color;
+}
+
+std::vector<ProvinceColor> religion_map_mode(const World& world) {
+    std::vector<ProvinceColor> province_color;
+    UI::Color min = UI::Color::rgb8(255, 255, 255);
+    for(unsigned int i = 0; i < world.provinces.size(); i++) {
+        Province* province = world.provinces[i];
+        std::unordered_map<Religion::Id, size_t> religion_amounts;
+        size_t total_amount = 0;
+        size_t max_amount = 0;
+        Religion::Id max_religion_id = 0;
+        for(unsigned int j = 0; j < province->pops.size(); j++) {
+            const Pop& pop = province->pops[j];
+            total_amount += pop.size;
+
+            auto search = religion_amounts.find(pop.religion->cached_id);
+            if(search == religion_amounts.end()) {
+                religion_amounts[pop.religion->cached_id] = pop.size;
+            }
+            else {
+                religion_amounts[pop.religion->cached_id] += pop.size;
+            }
+            size_t amount = religion_amounts[pop.religion->cached_id];
+            if(amount > max_amount) {
+                max_amount = amount;
+                max_religion_id = pop.religion->cached_id;
+            }
+        }
+        UI::Color max = UI::Color::rgba32(world.religions[max_religion_id]->color);
+        UI::Color color = UI::Color::lerp(min, max, max_amount / total_amount);
+        province_color.push_back(ProvinceColor(i, color));
+    }
+    // Water
+    province_color.push_back(ProvinceColor((Province::Id)-2, UI::Color::rgba32(0x00000000)));
+    // Land
+    province_color.push_back(ProvinceColor((Province::Id)-1, UI::Color::rgba32(0xffdddddd)));
     return province_color;
 }
