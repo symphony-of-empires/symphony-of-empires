1e3791fa0b78a90ce1a95136146dfd163786f1c5
superleaf1995 <jesusantonio30122016@gmail.com>
2021-12-23 12:33:18 -0600

    Imrpovments to the UI

--- src/client/interface/war.cpp
+++ src/client/interface/war.cpp
@@ -1,5 +1,7 @@
 #include "client/interface/war.hpp"
+#include "client/client_network.hpp"
 #include "io_impl.hpp"
+#include "action.hpp"
 
 using namespace Interface;
 
@@ -17,13 +19,14 @@ WarDeclarePrompt::WarDeclarePrompt(GameState& _gs, Nation* _nation)
     this->approve_btn->text("Declare war");
     this->approve_btn->on_click = ([](UI::Widget& w, void*) {
         auto& o = static_cast<WarDeclarePrompt&>(*w.parent);
-        delete w.parent;
+		o.gs.client->send(Action::DiploDeclareWar::form_packet(o.nation));
+        w.parent->kill();
     });
 
     this->deny_btn = new UI::Button(0, 48, this->width, 24, this);
     this->deny_btn->text("Nevermind");
     this->deny_btn->on_click = ([](UI::Widget& w, void*) {
         auto& o = static_cast<WarDeclarePrompt&>(*w.parent);
-        delete w.parent;
+        w.parent->kill();
     });
 }
