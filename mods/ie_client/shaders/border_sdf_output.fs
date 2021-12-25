b30bbfade84341f2e8b399cc0e9a8fe5bde1253c
Erik Jonasson <erikjonassonn@outlook.com>
2021-12-25 17:10:22 +0100

    Less VRAM usage

---
+++ mods/ie_client/shaders/border_sdf_output.fs
@@ -0,0 +1,13 @@
+#version 330 compatibility
+
+out vec4 f_frag_color;
+
+in vec2 v_texcoord;
+
+uniform sampler2D tex;
+
+void main() {
+	vec4 m_frag_data = texture(tex, v_texcoord);
+
+	f_frag_color = vec4(m_frag_data.z, 0., 0., 1.);
+}
