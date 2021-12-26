f4e1e39633ba026570389835fed7f7d96af67b27
superleaf1995 <jesusantonio30122016@gmail.com>
2021-12-25 18:18:54 -0600

    Left-side panel for better UI arrangement

--- src/client/interface/top_window.cpp
+++ src/client/interface/top_window.cpp
@@ -12,7 +12,7 @@
 using namespace Interface;
 
 TopWindow::TopWindow(GameState& _gs)
-    : UI::Group(0, 0, _gs.width, 128),
+    : UI::Group(0, 0, _gs.width, 256),
     gs{ _gs }
 {
     this->is_scroll = false;
@@ -27,7 +27,7 @@ TopWindow::TopWindow(GameState& _gs)
     policy_ibtn->tooltip->text("Laws & Policies");
 
     auto* economy_ibtn = new UI::Image(0, 0, 32, 32, &UnifiedRender::State::get_instance().tex_man->load(Path::get("ui/icons/economy_score.png")), this);
-    economy_ibtn->right_side_of(*policy_ibtn);
+    economy_ibtn->below_of(*policy_ibtn);
     economy_ibtn->on_click = (UI::Callback)([](UI::Widget& w, void*) {
         auto& o = static_cast<TopWindow&>(*w.parent);
         
@@ -36,7 +36,7 @@ TopWindow::TopWindow(GameState& _gs)
     economy_ibtn->tooltip->text("Economy & World Market");
 
     auto* military_ibtn = new UI::Image(0, 0, 32, 32, &UnifiedRender::State::get_instance().tex_man->load(Path::get("ui/icons/militar_score.png")), this);
-    military_ibtn->right_side_of(*economy_ibtn);
+    military_ibtn->below_of(*economy_ibtn);
     military_ibtn->on_click = (UI::Callback)([](UI::Widget& w, void*) {
         auto& o = static_cast<TopWindow&>(*w.parent);
         new Interface::ArmyView(o.gs);
@@ -45,7 +45,7 @@ TopWindow::TopWindow(GameState& _gs)
     military_ibtn->tooltip->text("Military");
 
     auto* research_ibtn = new UI::Image(0, 0, 32, 32, &UnifiedRender::State::get_instance().tex_man->load(Path::get("ui/icons/research.png")), this);
-    research_ibtn->right_side_of(*military_ibtn);
+    research_ibtn->below_of(*military_ibtn);
     research_ibtn->on_click = (UI::Callback)([](UI::Widget& w, void*) {
         auto& o = static_cast<TopWindow&>(*w.parent);
         new Interface::TechTreeView(o.gs);
@@ -53,19 +53,10 @@ TopWindow::TopWindow(GameState& _gs)
     research_ibtn->tooltip = new UI::Tooltip(research_ibtn, 512, 24);
     research_ibtn->tooltip->text("Research");
 
-    auto* exit_ibtn = new UI::Image(width / 2, 0, 32, 32, &UnifiedRender::State::get_instance().tex_man->load(Path::get("ui/icons/exit.png")), this);
-    exit_ibtn->on_click = (UI::Callback)([](UI::Widget& w, void*) {
-        auto& o = static_cast<TopWindow&>(*w.parent);
-        o.gs.run = false;
-    });
-    exit_ibtn->tooltip = new UI::Tooltip(exit_ibtn, 512, 24);
-    exit_ibtn->tooltip->text("Exits");
-
     auto* save_ibtn = new UI::Image(0, 0, 32, 32, &UnifiedRender::State::get_instance().tex_man->load(Path::get("ui/icons/save.png")), this);
-    save_ibtn->left_side_of(*exit_ibtn);
+    save_ibtn->below_of(*research_ibtn);
     save_ibtn->on_click = (UI::Callback)([](UI::Widget& w, void*) {
         auto& o = static_cast<TopWindow&>(*w.parent);
-
         Archive ar = Archive();
         ::serialize(ar, o.gs.world);
         ar.to_file(o.gs.curr_nation->ref_name + ".scv");
@@ -74,8 +65,16 @@ TopWindow::TopWindow(GameState& _gs)
     save_ibtn->tooltip = new UI::Tooltip(save_ibtn, 512, 24);
     save_ibtn->tooltip->text("Saves the current game; TODO: SAVE LUA STATE");
 
-    auto* speed0_btn = new UI::Button(0, 0, 48, 24, this);
-    speed0_btn->below_of(*military_ibtn);
+    auto* exit_ibtn = new UI::Image(0, 0, 32, 32, &UnifiedRender::State::get_instance().tex_man->load(Path::get("ui/icons/exit.png")), this);
+    exit_ibtn->below_of(*save_ibtn);
+    exit_ibtn->on_click = (UI::Callback)([](UI::Widget& w, void*) {
+        auto& o = static_cast<TopWindow&>(*w.parent);
+        o.gs.run = false;
+    });
+    exit_ibtn->tooltip = new UI::Tooltip(exit_ibtn, 512, 24);
+    exit_ibtn->tooltip->text("Exits");
+
+    auto* speed0_btn = new UI::Button(32, 0, 48, 24, this);
     speed0_btn->text("||");
     speed0_btn->on_click = ([](UI::Widget& w, void*) {
         auto& o = static_cast<TopWindow&>(*w.parent);
@@ -85,7 +84,6 @@ TopWindow::TopWindow(GameState& _gs)
     speed0_btn->tooltip->text("Pause");
 
     auto* speed1_btn = new UI::Button(0, 0, 48, 24, this);
-    speed1_btn->below_of(*military_ibtn);
     speed1_btn->right_side_of(*speed0_btn);
     speed1_btn->text(">");
     speed1_btn->on_click = ([](UI::Widget& w, void*) {
@@ -97,7 +95,6 @@ TopWindow::TopWindow(GameState& _gs)
     speed1_btn->tooltip->text("Turtle speed");
 
     auto* speed2_btn = new UI::Button(0, 0, 48, 24, this);
-    speed2_btn->below_of(*military_ibtn);
     speed2_btn->right_side_of(*speed1_btn);
     speed2_btn->text(">>");
     speed2_btn->on_click = ([](UI::Widget& w, void*) {
@@ -109,7 +106,6 @@ TopWindow::TopWindow(GameState& _gs)
     speed2_btn->tooltip->text("Horse speed");
 
     auto* speed3_btn = new UI::Button(0, 0, 48, 24, this);
-    speed3_btn->below_of(*military_ibtn);
     speed3_btn->right_side_of(*speed2_btn);
     speed3_btn->text(">>>");
     speed3_btn->on_click = ([](UI::Widget& w, void*) {
