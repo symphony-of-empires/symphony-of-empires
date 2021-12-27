b24666c365dc4f1643e8f642169d07e4381b0705
superleaf1995 <jesusantonio30122016@gmail.com>
2021-12-27 09:37:05 -0600

    Update API
    Signed-off-by: superleaf1995 <jesusantonio30122016@gmail.com>

--- mods/ie_core/scripts/api.lua
+++ mods/ie_core/scripts/api.lua
@@ -284,13 +284,7 @@ function Nation:get_allies()
 	return new_table
 end
 function Nation:is_ally(nation)
-	local table = Nation:get_allies(self)
-	for k, v in pairs(table) do
-		if v.id == nation.id then
-			return true
-		end
-	end
-	return false
+	return Nation:get_relation(nation).has_alliance
 end
 
 function Nation:get_war_enemies()
@@ -320,13 +314,7 @@ function Nation:get_embargoed()
 	return new_table
 end
 function Nation:is_embargoed(nation)
-	local table = Nation:get_embargoed(self)
-	for k, v in pairs(table) do
-		if v.id == nation.id then
-			return true
-		end
-	end
-	return false
+	return Nation:get_relation(nation).has_embargo
 end
 
 function Nation:get_owned_provinces()
@@ -362,6 +350,52 @@ function Nation:is_owns_nuclei_from(other)
 		end
 	end
 	return (total_eq_cont == nuclei_cont)
+end
+
+NationRelation = {
+	relation = 0.0,
+	interest = 0.0,
+	has_embargo = false,
+    has_war = false,
+    has_alliance = false,
+    has_defensive_pact = false,
+    has_truce = false,
+    has_embassy = false,
+    has_military_access = false,
+    has_market_access = false,
+    free_supplies = false
+}
+function NationRelation:new(o)
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+function Nation:get_relation(other)
+	rel = NationRelation:new{} or {}
+	rel.relation, rel.interest, rel.has_embargo, rel.has_war, rel.has_alliance, rel.has_defensive_pact, rel.has_truce, rel.has_embassy, rel.has_military_access, rel.has_market_access = get_nation_relation(self.id, other.id)
+	return rel
+end
+function Nation:set_relation(other, rel)
+	get_nation_relation(self.id, other.id, rel.relation, rel.interest, rel.has_embargo, rel.has_war, rel.has_alliance, rel.has_defensive_pact, rel.has_truce, rel.has_embassy, rel.has_military_access, rel.has_market_access)
+end
+function Nation:make_ally(other)
+	rel = Nation:get_relation(self, other)
+	rel.has_embargo = false
+	rel.has_truce = false
+	rel.has_war = false
+	rel.has_alliance = true
+	Nation:set_relation(self, other, rel)
+
+	-- TODO: Clear up the war
+end
+function Nation:embargo(other)
+	rel = Nation:get_relation(self, other)
+	rel.has_embargo = true
+	Nation:set_relation(self, other, rel)
+end
+function Nation:declare_unjustified_war(other)
+	nation_declare_unjustified_war(self.id, other.id)
 end
 
 Pop = {
