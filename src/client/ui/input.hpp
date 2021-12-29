ef1f12389893f556bd99b54b927e2211b78589ed
superleaf1995 <jesusantonio30122016@gmail.com>
2021-12-28 19:20:22 -0600

    Precompiled headers for faster compilation
    Signed-off-by: superleaf1995 <jesusantonio30122016@gmail.com>

--- src/client/ui/input.hpp
+++ src/client/ui/input.hpp
@@ -1,41 +1,42 @@
 #pragma once
 
 #include <string>
-
 #include "client/ui/widget.hpp"
 
 namespace UI {
 	class Context;
-	class Input: public Widget {
+	class Input : public Widget {
 	public:
+		Input(int x, int y, unsigned w, unsigned h, Widget* parent = nullptr);
+		virtual ~Input() override {};
+		
 		static void on_click_default(Widget& w, void*) {
-			Input& input = static_cast<Input&>(w);
+			Input& input = static_cast<UI::Input&>(w);
 			input.is_selected = true;
-		}
+		};
+
 		static void on_click_outside_default(Widget& w, void*) {
-			Input& input = static_cast<Input&>(w);
+			Input& input = static_cast<UI::Input&>(w);
 			if(input.is_selected) {
 				input.text(input.buffer);
 			}
 			input.is_selected = false;
-		}
+		};
+
 		static void on_update_default(Widget& w, void*) {
-			Input& input = static_cast<Input&>(w);
+			UI::Input& input = static_cast<UI::Input&>(w);
 			input.timer = (input.timer + 1) % 60;
 			std::string cursor = input.timer >= 30 ? "_" : "";
 			if(input.is_selected && input.timer % 30 == 0) {
 				if(!input.buffer.empty()) {
 					input.text(input.buffer + cursor);
-				}
-				else if(!cursor.empty()) {
+				} else if(!cursor.empty()) {
 					input.text(cursor);
 				}
 			}
-		}
-		Input(int x, int y, unsigned w, unsigned h, Widget* parent = nullptr);
-		virtual ~Input() override {};
+		};
 
-		std::function<void(Input&, const char*, void*)> on_textinput;
+		std::function<void(UI::Input&, const char*, void*)> on_textinput;
 		std::string buffer = "";
 		bool is_selected = false;
 	private:
--- src/unified_render/rectangle.cpp
+++ src/unified_render/rectangle.cpp
@@ -1,11 +1,9 @@
 #include "unified_render/rectangle.hpp"
 
-using namespace UnifiedRender;
-
-Rectangle Rectangle::intersection(Rectangle rect) {
+UnifiedRender::Rectangle UnifiedRender::Rectangle::intersection(UnifiedRender::Rectangle rect) {
     float i_left = glm::max(this->left, rect.left);
     float i_top = glm::max(this->top, rect.top);
     float i_right = glm::min(this->right, rect.right);
     float i_bottom = glm::min(this->bottom, rect.bottom);
-    return Rectangle{i_left, i_top, i_right - i_left, i_bottom - i_top};
+    return UnifiedRender::Rectangle{i_left, i_top, i_right - i_left, i_bottom - i_top};
 }
