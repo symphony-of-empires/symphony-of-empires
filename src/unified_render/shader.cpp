54747d4d2e25c340d886f27156655d5df20de2b5
coderguy57 <erikjonassonn@outlook.com>
2022-01-02 20:26:16 +0100

    Tiny fix

--- src/unified_render/shader.cpp
+++ src/unified_render/shader.cpp
@@ -247,7 +247,7 @@ std::string UnifiedRender::OpenGL::GLSL_Context::to_text(void) {
     for(const auto& tok : tokens) {
         switch(tok.type) {
         case GLSL_TokenType::MACRO:
-            end_buffer += tok.data + "\r\n";
+            end_buffer += "#" + tok.data + "\r\n";
             break;
         case GLSL_TokenType::SEMICOLON:
             end_buffer += ";\r\n";
