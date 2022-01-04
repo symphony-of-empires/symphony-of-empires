ff45f8c1a4b9f1f19e72112a99b0e1b8167cf22a
superleaf1995 <jesusantonio30122016@gmail.com>
2022-01-04 13:48:27 -0600

    License disclaimer on lua files
    Signed-off-by: superleaf1995 <jesusantonio30122016@gmail.com>

--- mods/ie_core/scripts/boat_types.lua
+++ mods/ie_core/scripts/boat_types.lua
@@ -1,3 +1,28 @@
+-- Industrial_Era_mod - Base game files for Symphony of Empires
+-- Copyright (C) 2021, Symphony of Empires contributors
+-- 
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program; if not, write to the Free Software Foundation, Inc.,
+-- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--
+-- ----------------------------------------------------------------------------
+--	Name:
+--  	boat_types.lua
+--
+-- 	Abstract:
+--      Does important stuff
+-- ----------------------------------------------------------------------------
+
 -- Naval unit types
 clipper = BoatType:new{ ref_name = "clipper", defense = 10.0, attack = 10.0, health = 100.0, speed = 0.5, capacity = 4000 }
 clipper.name = _("Clipper")
--- mods/ie_core/scripts/events/catholic_relief_act.lua
+++ mods/ie_core/scripts/events/catholic_relief_act.lua
@@ -1,3 +1,28 @@
+-- Industrial_Era_mod - Base game files for Symphony of Empires
+-- Copyright (C) 2021, Symphony of Empires contributors
+-- 
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program; if not, write to the Free Software Foundation, Inc.,
+-- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--
+-- ----------------------------------------------------------------------------
+--	Name:
+--  	scripts/events/catholic_relief_act.lua
+--
+-- 	Abstract:
+--      Does important stuff
+-- ----------------------------------------------------------------------------
+
 -- Catholic Relief Act of 1829
 -- https://en.wikipedia.org/wiki/Roman_Catholic_Relief_Act_1829
 function catholic_relief_act_of_1829_test()
