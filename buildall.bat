01da9d19b23d017c69d9ceea6be38beea44a6081
superleaf1995 <jesusantonio30122016@gmail.com>
2022-01-06 20:20:12 -0600

    Make a better build script
    Signed-off-by: superleaf1995 <jesusantonio30122016@gmail.com>

---
+++ buildall.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+
+MKDIR build
+CD build
+
+ECHO ">>> Making the release build"
+MKDIR release
+CD release
+cmake -DCMAKE_BUILD_TYPE=Release -DUR_BACKEND_OPENGL=1 -G"Unix Makefiles" ../..
+make -j3
+CD ..
+
+ECHO ">>> Making the debug build"
+MKDIR debug
+CD debug
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUR_BACKEND_OPENGL=1 -G"Unix Makefiles" ../..
+make -j3
+CD ..
+
+ECHO ">>> Creating distrobution package"
+MKDIR distro
+CD distro
+COPY ..\release\game\game.exe soe.exe
+STRIP --strip-debug soe.exe -o soe.exe
+COPY ..\release\unified_render\libunified_render.dll libunified_render.dll
+MKDIR mods
+XCOPY ..\..\mods mods /E /Y
+ECHO "Now add the remaining DLLs!"
+CD ..
+
+CD ..
