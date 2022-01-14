ab3e462ca7531aa57286a3b92bb31300aa608aa4
superleaf1995 <jesusantonio30122016@gmail.com>
2022-01-13 20:42:04 -0600

    Minor formatting stuff
    Signed-off-by: superleaf1995 <jesusantonio30122016@gmail.com>

--- game/src/client/interface/profiler_view.cpp
+++ game/src/client/interface/profiler_view.cpp
@@ -88,17 +88,16 @@ ProfilerView::ProfilerView(GameState& _gs)
             for(size_t i = task_views.size(); i < tasks.size(); i++) {
                 task_views.push_back(new ProfilerTaskView(&state, 10, 50 + i * 25));
             }
-        }
-        else if(task_views.size() > tasks.size()) {
+        } else if(task_views.size() > tasks.size()) {
             for(size_t i = tasks.size(); i < task_views.size(); i++) {
                 task_views[i]->kill();
             }
             task_views.erase(task_views.begin() + tasks.size(), task_views.end());
         }
+		
         for(size_t i = 0; i < task_views.size(); i++) {
             task_views[i]->set_task(tasks[i]);
         }
-
     });
 }
 
