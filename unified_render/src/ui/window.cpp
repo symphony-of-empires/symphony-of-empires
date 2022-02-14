61bc92b2224cb7ca37faac7678d76bb99166d5d6
coderguy57 <erikjonassonn@outlook.com>
2022-02-14 13:55:23 +0100

    Updated main menu

--- unified_render/src/ui/window.cpp
+++ unified_render/src/ui/window.cpp
@@ -42,5 +42,5 @@ Window::Window(int _x, int _y, unsigned w, unsigned h, Widget* _parent)
     glm::ivec2 size(4, 4);
     glm::ivec2 texture_size(10, 10);
     glm::ivec2 offset(0, 24);
-    border = new Border(g_ui_context->border_tex, size, texture_size, offset);
+    border = Border(g_ui_context->border_tex, size, texture_size, offset);
 }
