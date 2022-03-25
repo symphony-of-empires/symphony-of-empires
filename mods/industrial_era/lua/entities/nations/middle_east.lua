7c75d8331448d57e84233767bf6079f1c9936e7a
superleaf1995 <jesusantonio30122016@gmail.com>
2022-03-24 21:08:12 -0700

    Add many of the yemen OPMs

--- mods/industrial_era/lua/entities/nations/middle_east.lua
+++ mods/industrial_era/lua/entities/nations/middle_east.lua
@@ -2,22 +2,94 @@ najran = Nation:new{ ref_name = "najran", name = _("Najran") }
 najran.adjective = _("Najrani")
 najran:register()
 najran:add_accepted_culture(arabic)
-najran:add_client_hint(monarchy, _("Najran"), 0x808080)
+najran:add_client_hint(monarchy, _("Najran"), 0xDEED82)
 
 nation = Nation:new{ ref_name = "zaidi_imamate", name = _("Zaidi Imamate") }
 nation.adjective = _("Zaidi")
 nation:register()
 nation:add_accepted_culture(arabic)
-nation:add_client_hint(monarchy, _("Zaidi Imamate"), 0x808080)
+nation:add_client_hint(monarchy, _("Zaidi Imamate"), 0x8e332f)
 
 nation = Nation:new{ ref_name = "oman", name = _("Oman") }
 nation.adjective = _("Omani")
 nation:register()
 nation:add_accepted_culture(arabic)
-nation:add_client_hint(monarchy, _("Oman"), 0x808080)
+nation:add_client_hint(monarchy, _("Oman"), 0x3B3E69)
 
-nation = Nation:new{ ref_name = "nejd", name = _("Emirate of Nejd") }
+nation = Nation:new{ ref_name = "nejd", name = _("Nejd") }
 nation.adjective = _("Nejdi")
 nation:register()
 nation:add_accepted_culture(arabic)
-nation:add_client_hint(monarchy, _("Emirate of Nejd"), 0x808080)
+nation:add_client_hint(monarchy, _("Nejd"), 0x314E2A)
+
+nation = Nation:new{ ref_name = "mahra", name = _("Mahra") }
+nation.adjective = _("Mahrai")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Mahra"), 0x52AC3B)
+
+nation = Nation:new{ ref_name = "muscat", name = _("Muscat") }
+nation.adjective = _("Muscati")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Muscat"), 0xE77A35)
+
+nation = Nation:new{ ref_name = "kasadi", name = _("Kasadi") }
+nation.adjective = _("Kasadi")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Kasadi"), 0xE73537)
+
+nation = Nation:new{ ref_name = "mahwali", name = _("Mahwali") }
+nation.adjective = _("Mahwali")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Mahwali"), 0x8A35E7)
+
+nation = Nation:new{ ref_name = "wahidi", name = _("Wahidi") }
+nation.adjective = _("Wahidi")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Wahidi"), 0x3599E7)
+
+nation = Nation:new{ ref_name = "wadi_hajr", name = _("Wadi Hajr") }
+nation.adjective = _("Wadi Hajr")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Wadi Hajr"), 0xC2E735)
+
+nation = Nation:new{ ref_name = "tamim", name = _("Tamim") }
+nation.adjective = _("Tamim")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Tamim"), 0xA71256)
+
+nation = Nation:new{ ref_name = "hamum", name = _("Hamum") }
+nation.adjective = _("Hamum")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Hamum"), 0x63D03E)
+
+nation = Nation:new{ ref_name = "aulaqi", name = _("Aulaqi") }
+nation.adjective = _("Aulaqi")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Aulaqi"), 0xd0cc3e)
+
+nation = Nation:new{ ref_name = "fadhli", name = _("Fadhli") }
+nation.adjective = _("Fadhli")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Fadhli"), 0x53f1f6)
+
+nation = Nation:new{ ref_name = "beihan", name = _("Beihan") }
+nation.adjective = _("Beihani")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Beihan"), 0x682f8e)
+
+nation = Nation:new{ ref_name = "dhale", name = _("Dhale") }
+nation.adjective = _("Dhalian")
+nation:register()
+nation:add_accepted_culture(arabic)
+nation:add_client_hint(monarchy, _("Dhale"), 0xff00d0)
