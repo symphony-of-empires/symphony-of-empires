50b2d2f100d7f7a8898d1e64b1c46c8c1d781b58
superleaf1995 <jesusantonio30122016@gmail.com>
2022-04-01 10:29:34 -0700

    Register flags

--- mods/industrial_era/lua/entities/nations/oceania.lua
+++ mods/industrial_era/lua/entities/nations/oceania.lua
@@ -29,6 +29,9 @@ nation:register()
 nation:set_ideology(monarchy)
 --nation:add_accepted_culture(chinesse)
 nation:add_client_hint(monarchy, _("Aceh"), 0xC82344)
+nation:add_client_hint(fascism, _("Aceh"), 0xC82344)
+nation:add_client_hint(republic, _("Aceh"), 0xC82344)
+nation:add_client_hint(socialism, _("Aceh"), 0xC82344)
 
 nation = Nation:new{ ref_name = "johor", name = _("Johor") }
 nation.adjective = _("Johor")
@@ -36,6 +39,9 @@ nation:register()
 nation:set_ideology(monarchy)
 --nation:add_accepted_culture(chinesse)
 nation:add_client_hint(monarchy, _("Johor"), 0x896241)
+nation:add_client_hint(fascism, _("Johor"), 0x896241)
+nation:add_client_hint(republic, _("Johor"), 0x896241)
+nation:add_client_hint(socialism, _("Johor"), 0x896241)
 
 nation = Nation:new{ ref_name = "siam", name = _("Siam") }
 nation.adjective = _("Siam")
@@ -78,6 +84,9 @@ nation:register()
 nation:set_ideology(monarchy)
 --nation:add_accepted_culture(chinesse)
 nation:add_client_hint(monarchy, _("Sulu"), 0x374A82)
+nation:add_client_hint(fascism, _("Sulu"), 0x374A82)
+nation:add_client_hint(republic, _("Sulu"), 0x374A82)
+nation:add_client_hint(socialism, _("Sulu"), 0x374A82)
 
 nation = Nation:new{ ref_name = "bali", name = _("Bali") }
 nation.adjective = _("Bali")
