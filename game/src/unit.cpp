b55d06e45e7e1ae576e72a31f4cb5f884b8a372e
superleaf1995 <jesusantonio30122016@gmail.com>
2022-06-05 13:42:12 -0700

    dont make a copy of the entire vector

--- game/src/unit.cpp
+++ game/src/unit.cpp
@@ -126,9 +126,8 @@ void UnitManager::add_unit(Unit unit, Province::Id unit_current_province) {
 }
 
 void UnitManager::remove_unit(Unit::Id unit_id) {
-    Province::Id current_province_id = unit_province[unit_id];
-
-    auto units_in_province = province_units[current_province_id];
+    const Province::Id current_province_id = unit_province[unit_id];
+    auto& units_in_province = province_units[current_province_id];
     Eng3D::fast_erase(units_in_province, unit_id);
     units[unit_id].cached_id = Province::invalid();
 }
@@ -137,9 +136,8 @@ void UnitManager::move_unit(Unit::Id unit_id, Province::Id target_province_id) {
     assert(units[unit_id].can_move()); // Must be able to move to perform this...
     assert(unit_province[unit_id] != target_province_id); // Not setting to same province
 
-    Province::Id current_province_id = unit_province[unit_id];
-
-    auto units_in_province = province_units[current_province_id];
+    const Province::Id current_province_id = unit_province[unit_id];
+    auto& units_in_province = province_units[current_province_id];
     Eng3D::fast_erase(units_in_province, unit_id);
     unit_province[unit_id] = target_province_id;
     province_units[current_province_id].push_back(unit_id);
