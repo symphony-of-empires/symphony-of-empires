2a172a9f53f42aafac43de688df210a55cb58f76
wxwisiasdf <wxwisiasdf@debian-BULLSEYE-live-builder-AMD64>
2022-09-05 22:21:22 -0700

    Use Eng3D::Rand instead of std::rand on disc distro

--- eng3d/src/disc_dist.hpp
+++ eng3d/src/disc_dist.hpp
@@ -29,6 +29,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <vector>
+#include "eng3d/rand.hpp"
 
 /// @brief Uses the Aiias method to generate a lookup table of with different probabilties
 /// @tparam T The type of items to obtain the probability from
@@ -37,70 +38,72 @@
 template<typename T>
 class DiscreteDistribution {
 private:
-	std::vector<T> _items;
-	std::vector<float> alias;
-	std::vector<float> prob;
+    std::vector<T> _items;
+    std::vector<float> alias;
+    std::vector<float> prob;
+    Eng3D::Rand _rand;
 public:
-	DiscreteDistribution(std::vector<T> items, std::vector<float> probabilities)
-		: _items{ items }
-	{
-		// Scale each probabilty
-		float total = std::accumulate(probabilities.begin(), probabilities.end(), 0);
-		float scale = probabilities.size() / total;
-		for(auto& p : probabilities) p *= scale;
+    DiscreteDistribution(std::vector<T> items, std::vector<float> probabilities)
+        : _items{ items },
+        _rand{ 0 }
+    {
+        // Scale each probabilty
+        float total = std::accumulate(probabilities.begin(), probabilities.end(), 0);
+        float scale = probabilities.size() / total;
+        for(auto& p : probabilities) p *= scale;
 
-		// Fill two work tables with probabilities larger/smaller than 1
-		std::vector<std::pair<float, size_t>> small;
-		small.reserve(probabilities.size());
-		std::vector<std::pair<float, size_t>> big;
-		big.reserve(probabilities.size());
-		for(size_t i = 0; i < probabilities.size(); i++) {
-			if(probabilities[i] < 1.0f)
-				small.push_back(std::make_pair(probabilities[i], i));
-			else
-				big.push_back(std::make_pair(probabilities[i], i));
-		}
+        // Fill two work tables with probabilities larger/smaller than 1
+        std::vector<std::pair<float, size_t>> small;
+        small.reserve(probabilities.size());
+        std::vector<std::pair<float, size_t>> big;
+        big.reserve(probabilities.size());
+        for(size_t i = 0; i < probabilities.size(); i++) {
+            if(probabilities[i] < 1.0f)
+                small.emplace_back(std::make_pair(probabilities[i], i));
+            else
+                big.emplace_back(std::make_pair(probabilities[i], i));
+        }
 
-		// Remove from the bigger one and place on the smaller ones
-		alias.resize(probabilities.size(), 0);
-		prob.resize(probabilities.size(), 0);
-		while(small.size() > 0 && big.size() > 0) {
-			std::pair<float, size_t> less = small.back();
-			small.pop_back();
-			std::pair<float, size_t> greater = big.back();
-			big.pop_back();
-			prob[less.second] = less.first;
-			alias[less.second] = greater.second;
-			greater.first = (greater.first + less.first) - 1;
-			if(greater.first < 1.f)
-				small.push_back(greater);
-			else
-				big.push_back(greater);
-		}
+        // Remove from the bigger one and place on the smaller ones
+        alias.resize(probabilities.size(), 0);
+        prob.resize(probabilities.size(), 0);
+        while(!small.empty() && !big.empty()) {
+            const auto& less = small.back();
+            auto greater = big.back();
+            big.pop_back();
+            prob[less.second] = less.first;
+            alias[less.second] = greater.second;
+            small.pop_back();
+            greater.first = (greater.first + less.first) - 1;
+            if(greater.first < 1.f)
+                small.emplace_back(greater);
+            else
+                big.emplace_back(greater);
+        }
 
-		while(big.size() > 0) {
-			std::pair<float, int> greater = big.back();
-			big.pop_back();
-			prob[greater.second] = 1.f;
-		}
-		
-		while(small.size() > 0) {
-			std::pair<float, int> less = small.back();
-			small.pop_back();
-			prob[less.second] = 1.f;
-		}
-	}
-	
-	~DiscreteDistribution()
-	{
+        while(!big.empty()) {
+            const auto& greater = big.back();
+            prob[greater.second] = 1.f;
+            big.pop_back();
+        }
 
-	};
+        while(!small.empty()) {
+            const auto& less = small.back();
+            prob[less.second] = 1.f;
+            small.pop_back();
+        }
+    }
 
-	// Get a random item with a certian probabilty
-	T get_item() {
-		size_t index = std::rand() % _items.size();
-		float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-		if(prob[index] < r) index = alias[index];
-		return _items[index];
-	}
+    ~DiscreteDistribution()
+    {
+
+    };
+
+    // Get a random item with a certian probabilty
+    T get_item() {
+        size_t index = _rand() % _items.size();
+        float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        if(prob[index] < r) index = alias[index];
+        return _items[index];
+    }
 };
