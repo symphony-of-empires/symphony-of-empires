f49fcc2ca684d050028b514fe5baf8e1ea39b675
wxwisiasdf <wxwisiasdf@debian-BULLSEYE-live-builder-AMD64>
2022-09-20 19:54:36 -0700

    ActionHandleSr -> action_handle_sr

--- game/src/action.cpp
+++ game/src/action.cpp
@@ -39,7 +39,7 @@
 using namespace Action;
 
 template<ActionType type, typename Pred, typename ...Targs>
-Eng3D::Networking::Packet ActionHandlerSR(Pred p) {
+Eng3D::Networking::Packet action_handler_sr(Pred p) {
     Eng3D::Networking::Packet packet{};
     Archive ar{};
     ::serialize<ActionType>(ar, type);
@@ -49,7 +49,7 @@ Eng3D::Networking::Packet ActionHandlerSR(Pred p) {
 }
 
 Eng3D::Networking::Packet AiControl::form_packet(const Nation& nation) {
-    return ActionHandlerSR<ActionType::AI_CONTROL>([&nation](auto& ar) {
+    return action_handler_sr<ActionType::AI_CONTROL>([&nation](auto& ar) {
         ::serialize(ar, nation.ai_controlled);
         ::serialize(ar, nation.ai_do_cmd_troops);
         ::serialize(ar, nation.ai_controlled);
@@ -62,13 +62,13 @@ Eng3D::Networking::Packet AiControl::form_packet(const Nation& nation) {
 }
 
 Eng3D::Networking::Packet DiploDeclareWar::form_packet(const Nation& nation) {
-    return ActionHandlerSR<ActionType::DIPLO_DECLARE_WAR>([&nation](auto& ar) {
+    return action_handler_sr<ActionType::DIPLO_DECLARE_WAR>([&nation](auto& ar) {
         ::serialize(ar, &nation);
     });
 }
 
 Eng3D::Networking::Packet ProvinceUpdate::form_packet(const std::vector<Province>& list) {
-    return ActionHandlerSR<ActionType::PROVINCE_UPDATE>([&list](auto& ar) {
+    return action_handler_sr<ActionType::PROVINCE_UPDATE>([&list](auto& ar) {
         for(const auto& province : list) {
             auto& ref = province;
             ::serialize(ar, &ref); // ProvinceRef
@@ -78,7 +78,7 @@ Eng3D::Networking::Packet ProvinceUpdate::form_packet(const std::vector<Province
 }
 
 Eng3D::Networking::Packet NationUpdate::form_packet(const std::vector<Nation>& list) {
-    return ActionHandlerSR<ActionType::NATION_UPDATE>([&list](auto& ar) {
+    return action_handler_sr<ActionType::NATION_UPDATE>([&list](auto& ar) {
         for(const auto& nation : list) {
             auto& ref = nation;
             ::serialize(ar, &ref); // NationRef
@@ -88,13 +88,13 @@ Eng3D::Networking::Packet NationUpdate::form_packet(const std::vector<Nation>& l
 }
 
 Eng3D::Networking::Packet SelectNation::form_packet(const Nation& nation) {
-    return ActionHandlerSR<ActionType::SELECT_NATION>([&nation](auto& ar) {
+    return action_handler_sr<ActionType::SELECT_NATION>([&nation](auto& ar) {
         ::serialize(ar, &nation);
     });
 }
 
 Eng3D::Networking::Packet BuildingStartProducingUnit::form_packet(const Province& province, const BuildingType& building_type, const Nation& nation, const UnitType& unit_type) {
-    return ActionHandlerSR<ActionType::BUILDING_START_BUILDING_UNIT>([&](auto& ar) {
+    return action_handler_sr<ActionType::BUILDING_START_BUILDING_UNIT>([&](auto& ar) {
         ::serialize(ar, &province);
         ::serialize(ar, &building_type);
         ::serialize(ar, &nation);
@@ -103,40 +103,40 @@ Eng3D::Networking::Packet BuildingStartProducingUnit::form_packet(const Province
 }
 
 Eng3D::Networking::Packet BuildingAdd::form_packet(const Province& province, const BuildingType& building_type) {
-    return ActionHandlerSR<ActionType::BUILDING_ADD>([&province, &building_type](auto& ar) {
+    return action_handler_sr<ActionType::BUILDING_ADD>([&province, &building_type](auto& ar) {
         ::serialize(ar, &province);
         ::serialize(ar, &building_type);
     });
 }
 
 Eng3D::Networking::Packet FocusTech::form_packet(const Technology& technology) {
-    return ActionHandlerSR<ActionType::FOCUS_TECH>([&](auto& ar) {
+    return action_handler_sr<ActionType::FOCUS_TECH>([&](auto& ar) {
         ::serialize(ar, &technology);
     });
 }
 
 Eng3D::Networking::Packet NationAdd::form_packet(const Nation& nation) {
-    return ActionHandlerSR<ActionType::NATION_ADD>([&](auto& ar) {
+    return action_handler_sr<ActionType::NATION_ADD>([&](auto& ar) {
         ::serialize(ar, nation);
     });
 }
 
 Eng3D::Networking::Packet NationTakeDecision::form_packet(const Event& event, const Decision& decision) {
-    return ActionHandlerSR<ActionType::NATION_TAKE_DECISION>([&](auto& ar) {
+    return action_handler_sr<ActionType::NATION_TAKE_DECISION>([&](auto& ar) {
         ::serialize(ar, event);
         ::serialize(ar, decision.ref_name);
     });
 }
 
 Eng3D::Networking::Packet UnitAdd::form_packet(const Unit& unit) {
-    return ActionHandlerSR<ActionType::UNIT_ADD>([&](auto& ar) {
+    return action_handler_sr<ActionType::UNIT_ADD>([&](auto& ar) {
         ::serialize(ar, unit);
         ::serialize<Province::Id>(ar, unit.province_id());
     });
 }
 
 Eng3D::Networking::Packet UnitUpdate::form_packet(const std::vector<Unit>& units) {
-    return ActionHandlerSR<ActionType::UNIT_ADD>([&](auto& ar) {
+    return action_handler_sr<ActionType::UNIT_ADD>([&](auto& ar) {
         ::serialize<Unit::Id>(ar, units.size());
         for(const auto& unit : units)
             ::serialize(ar, unit);
@@ -144,13 +144,13 @@ Eng3D::Networking::Packet UnitUpdate::form_packet(const std::vector<Unit>& units
 }
 
 Eng3D::Networking::Packet UnitRemove::form_packet(const Unit& unit) {
-    return ActionHandlerSR<ActionType::UNIT_ADD>([&](auto& ar) {
+    return action_handler_sr<ActionType::UNIT_ADD>([&](auto& ar) {
         ::serialize<Unit::Id>(ar, unit.get_id());
     });
 }
 
 Eng3D::Networking::Packet UnitMove::form_packet(const Unit& unit, const Province& province) {
-    return ActionHandlerSR<ActionType::UNIT_ADD>([&](auto& ar) {
+    return action_handler_sr<ActionType::UNIT_ADD>([&](auto& ar) {
         ::serialize<Unit::Id>(ar, unit.get_id());
         ::serialize<Province::Id>(ar, province.get_id());
     });
