2d4367ffa5520474a905803b6cebb9a4f10a7686
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-01-13 13:01:25 +0800

    Add renderbuffers

---
+++ eng3d/eng3d/renderbuffer.hpp
@@ -0,0 +1,39 @@
+// Eng3D - General purpouse game engine
+// Copyright (C) 2021, Eng3D contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Name:
+//      renderbuffer.hpp
+//
+// Abstract:
+//      Does some important stuff.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+namespace Eng3D {
+    namespace OpenGL {
+        class Renderbuffer {
+            unsigned int id = 0;
+        public:
+            Renderbuffer();
+            ~Renderbuffer();
+            void use();
+        private:
+        };
+    }
+    using Renderbuffer = OpenGL::Renderbuffer;
+}
