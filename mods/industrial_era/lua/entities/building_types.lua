7c6576bedc5dedfbd0be98c778bd1c61b308a9aa
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-01-23 07:55:09 +0800

    Remove outputs for non-industrial buildings

--- mods/industrial_era/lua/entities/building_types.lua
+++ mods/industrial_era/lua/entities/building_types.lua
@@ -26,16 +26,9 @@
 -- Outpost types
 barracks = BuildingType:new{ ref_name = "barracks", name = _("Barracks"), is_build_land_units = true, defense_bonus = 1.05 }
 barracks:register()
-barracks:add_output(Commodity:get("arms"))
-
 fort = BuildingType:new{ ref_name = "fort", name = _("Fort"), defense_bonus = 1.45 }
 fort:register()
-fort:add_output(Commodity:get("arms"))
-
 seaport = BuildingType:new{ ref_name = "seaport", name = _("Seaport"), is_build_naval_units = true, defense_bonus = 1.05 }
 seaport:register()
-seaport:add_output(Commodity:get("arms"))
-
-coast_cannons = BuildingType:new{ ref_name = "coast_cannons", name = _("Coast cannons"), defense_bonus = 1.01 }
+coast_cannons = BuildingType:new{ ref_name = "coast_cannons", name = _("Coast cannons"), defense_bonus = 1.50 }
 coast_cannons:register()
-coast_cannons:add_output(Commodity:get("arms"))
